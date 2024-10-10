12e8bd345eb1dff1eca2c2e4b0845646f6af2e49
Wojtic <vojtacerny012@gmail.com>
2024-10-10 22:45:01 +0200

    Add "parallax"

--- client/main.js
+++ client/main.js
@@ -2,6 +2,18 @@ let scrolling = false;
 let lastPage = 0;
 const N = 5;
 
+let cursorX = 0;
+let cursorY = 0;
+
+let vw = Math.max(
+  document.documentElement.clientWidth || 0,
+  window.innerWidth || 0
+);
+let vh = Math.max(
+  document.documentElement.clientHeight || 0,
+  window.innerHeight || 0
+);
+
 document.addEventListener("scroll", (event) => {
   update(getScrollPercent());
 });
@@ -11,6 +23,22 @@ window.onload = () => {
     .getElementById("scroll_page4")
     .scrollIntoView({ behavior: "smooth" });
 };
+
+document.onmousemove = handleMouseMove;
+function handleMouseMove(event) {
+  vw = Math.max(
+    document.documentElement.clientWidth || 0,
+    window.innerWidth || 0
+  );
+  vh = Math.max(
+    document.documentElement.clientHeight || 0,
+    window.innerHeight || 0
+  );
+  cursorX = event.clientX / vw - 0.5;
+  cursorY = event.clientY / vh - 0.5;
+
+  update(getScrollPercent());
+}
 
 function getScrollPercent() {
   var h = document.documentElement,
@@ -32,6 +60,11 @@ function update(scrollPercent) {
   let scale = 1;
   for (let i = 0; i < N - page; i++) {
     document.querySelector("#page" + (page + i)).style.display = "inherit";
+    document.querySelector("#page" + (page + i)).style.translate =
+      Math.round(-cursorX * vw * i * 0.05) +
+      "px " +
+      Math.round(-cursorY * vh * i * 0.05) +
+      "px";
     document.querySelector("#page" + (page + i)).style.transform =
       "scale(" + 1 / linear(-scrollPercent * 4 + page + i + 1) + ")";
     scale /= 3;
