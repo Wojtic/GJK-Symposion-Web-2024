1e24c6294cc79ea45f05b5555ad656433bcbcaf2
Wojtic <vojtacerny012@gmail.com>
2024-10-16 17:44:13 +0200

    Rewrite js

--- client/main.js
+++ client/main.js
@@ -1,7 +1,6 @@
-let scrolling = false;
-let lastPage = 0;
-const N = 9*2-1;
-const VISIBLE_PAGES = 4;
+const N = 6;
+const N_SCROLL = 15;
+let page = 0;
 
 let cursorX = 0;
 let cursorY = 0;
@@ -9,64 +8,39 @@ let cursorY = 0;
 const body = document.body;
 const html = document.documentElement;
 
-let vw = Math.max(
-  html.clientWidth || 0,
-  window.innerWidth || 0
-);
-let vh = Math.max(
-  html.clientHeight || 0,
-  window.innerHeight || 0
-);
+let vw = Math.max(html.clientWidth || 0, window.innerWidth || 0);
+let vh = Math.max(html.clientHeight || 0, window.innerHeight || 0);
 
 document.addEventListener("scroll", (event) => {
   update(getScrollPercent());
 });
 
-function copyPage() {
-  toPaste = document.getElementsByClassName("copy");
-  for(var i = 0; i < toPaste.length; i++){
-    toPaste[i].innerHTML = document.getElementById(toPaste[i].id.split("_")[2]).innerHTML;
-    toPaste[i].id = toPaste[i].id.split("_")[0];
-  }
-}
-
 function assignZIndex() {
-  pages = document.getElementsByClassName("page");
-  console.log(pages);
-  for(var i = 0; i < pages.length; i++){
-    pages[i].style.zIndex = (pages.length - i + 5).toString();
+  for (let i = 0; i < N; i++) {
+    document.querySelector("#page" + i).style.zIndex = (N + page - i - 1) % N;
   }
 }
 
-document.addEventListener("DOMContentLoaded", function(event) { 
-  if ('scrollRestoration' in history) {
-    history.scrollRestoration = 'manual';
+document.addEventListener("DOMContentLoaded", function (event) {
+  if ("scrollRestoration" in history) {
+    history.scrollRestoration = "manual";
   }
-  copyPage();
   html.style.scrollBehavior = "initial";
-  document
-    .getElementById("scroll_page5")
-    .scrollIntoView();
+  document.getElementById("scroll_page8").scrollIntoView();
   html.style.scrollBehavior = "smooth";
   assignZIndex();
   update(getScrollPercent());
   cover = document.getElementById("cover");
   cover.style.animation = "fade_out 1s linear 1";
-  cover.addEventListener('animationend', () => {
+  cover.addEventListener("animationend", () => {
     cover.style.display = "none";
   });
 });
 
 document.onmousemove = handleMouseMove;
 function handleMouseMove(event) {
-  vw = Math.max(
-    html.clientWidth || 0,
-    window.innerWidth || 0
-  );
-  vh = Math.max(
-    html.clientHeight || 0,
-    window.innerHeight || 0
-  );
+  vw = Math.max(html.clientWidth || 0, window.innerWidth || 0);
+  vh = Math.max(html.clientHeight || 0, window.innerHeight || 0);
   cursorX = event.clientX / vw - 0.5;
   cursorY = event.clientY / vh - 0.5;
 
@@ -82,11 +56,16 @@ function getScrollPercent() {
 }
 
 function update(scrollPercent) {
-  if (scrollPercent == 1 || scrollPercent == 0){ //FIX: Needed for mobile, can't scroll to the bottom
+  const scrolledPage = Math.round((N_SCROLL - 1) * scrollPercent);
+  const dist =
+    (N_SCROLL - 1) * scrollPercent - Math.round((N_SCROLL - 1) * scrollPercent);
+  if (
+    Math.abs(dist) < 0.01 &&
+    (scrolledPage >= N_SCROLL - 2 || scrolledPage <= 1)
+  ) {
+    //FIX: Needed for mobile, can't scroll to the bottom
     html.style.scrollBehavior = "initial";
-    document
-      .getElementById("scroll_page5")
-      .scrollIntoView();
+    document.getElementById("scroll_page7").scrollIntoView();
     html.style.scrollBehavior = "smooth";
   }
 
@@ -94,33 +73,35 @@ function update(scrollPercent) {
 }
 
 function setCSS(scrollPercent) {
-  const page = Math.round((N - 1) * scrollPercent);
-  const dist = (N - 1) * scrollPercent - page;
+  page = Math.round((N_SCROLL - 1) * scrollPercent) % N;
   const linear = (x) => 0.5 * x + 0.5;
 
-  let scale = 1;
-  for (let i = 0; i < VISIBLE_PAGES; i++) {
-    if (page + i >= N) {continue}
-    document.querySelector("#page" + (page + i)).style.display = "inherit";
-    document.querySelector("#page" + (page + i)).style.opacity = i == 0 ? Math.min(
-      1,
-      1 - 2 * dist
-    ) : "1";
-    document.querySelector("#page" + (page + i)).style.translate =
-      Math.round(-cursorX * vw * i * 0.05) +
+  assignZIndex();
+
+  for (let j = 0; j < N; j++) {
+    let curPage = (N + page + j) % N;
+    const scale =
+      1 /
+      linear(
+        -scrollPercent * (N_SCROLL - 1) +
+          Math.round((N_SCROLL - 1) * scrollPercent) +
+          j +
+          1
+      );
+
+    const dist =
+      (N_SCROLL - 1) * scrollPercent -
+      Math.round((N_SCROLL - 1) * scrollPercent);
+
+    document.querySelector("#page" + curPage).style.display = "inherit";
+    document.querySelector("#page" + curPage).style.opacity =
+      j == 0 ? Math.min(1, 1 - 2 * dist) : "1";
+    document.querySelector("#page" + curPage).style.translate =
+      Math.round(-cursorX * vw * j * 0.05) +
       "px " +
-      Math.round(-cursorY * vh * i * 0.05) +
+      Math.round(-cursorY * vh * j * 0.05) +
       "px";
-    document.querySelector("#page" + (page + i)).style.transform =
-      "scale(" + 1 / linear(-scrollPercent * (N-1) + page + i + 1) + ")";
-    scale /= 3;
-  }
-
-  for (let i = page + VISIBLE_PAGES; i < N; i++) {
-    document.querySelector("#page" + i).style.display = "none";
-  }
-
-  for (let i = 0; i < page; i++) {
-    document.querySelector("#page" + i).style.display = "none";
+    document.querySelector("#page" + curPage).style.transform =
+      "scale(" + scale + ")";
   }
 }
