78c0e2f0436b9cefcd30e081c77aa0e97c0c98f1
Wojtic <vojtacerny012@gmail.com>
2024-10-31 23:44:10 +0100

    Filter out -

--- API/main.py
+++ API/main.py
@@ -25,6 +25,7 @@ class Harmonogram:
     def fetch_data(self):
         df = pd.read_csv(URL)
         self.names = df.iloc[:, 4].astype(str).tolist()
+        self.names = [name if name != "-" else "" for name in self.names]
         self.times = df.iloc[:, 5].astype(str).tolist()
         self.rooms = df.iloc[:, 6].astype(str).tolist()
         self.medailons = df.iloc[:, 7].astype(str).tolist()
